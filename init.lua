cacf0f4826e4c60cb40c2a8e9ae5de723b36d8cd
Dheisom Gomes <dheisomgomes0@gmail.com>
2022-01-27 22:17:03 -0300

    Plugin name added on every log and autoload plugin on install finish

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- mod-version:2
 
 local core = require 'core'
+local config = require 'core.config'
 local command = require 'core.command'
 local common = require 'core.common'
 local process = require 'process'
@@ -20,22 +21,26 @@ local function get_and_show_list()
     coroutine.yield(2)
   end
   local data = proc:read_stdout(1 * 1048576) -- 1MiB max
+  if data == nil then
+    core.log("[PluginManager] No data received, it can be a network problem!")
+    return
+  end
   local plugins = util.get_plugins(data)
   coroutine.yield(2)
   if unpack(plugins) then
-    core.log("Failed to load plugin list!")
+    core.log("[PluginManager] Failed to load plugin list!")
   else
     core.command_view:enter(
       "Install Plugin",
       function(text, item)
         local text = item and item.text or text
         if text == "" then
-          core.log("Operation cancelled!")
+          core.log("[PluginManager] Operation cancelled!")
           return
         end
         local space = text:find(" ")
         local name = text:sub(1, space-1)
-        core.log("Installing " .. name .. "...")
+        core.log("[PluginManager] Installing " .. name .. "...")
         local plugin = plugins[name]
         core.add_thread(function()
           local url = plugin.path
@@ -51,7 +56,8 @@ local function get_and_show_list()
             end
             coroutine.yield(2)
           end
-          core.log("Plugin '"..name.."' installed, reload your editor")
+          config.plugins[name] = require('plugins.'..name)
+          core.log("[PluginManager] Plugin '"..name.."' installed and loaded")
         end)
       end,
       function(text)
@@ -68,7 +74,7 @@ end
 
 command.add(nil, {
     ["PluginManager:install-plugin"] = function()
-      core.log("Loading plugin list...")
+      core.log("[PluginManager] Loading plugin list...")
       core.add_thread(get_and_show_list)
     end
   }
